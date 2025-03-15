6270e1f10e8e829af4de401352e5058a99d75de2
PierreAdmin <40363564+PierreAdmin@users.noreply.github.com>
2025-03-15 12:59:58 +0100

    Add files via upload

---
+++ function.js
@@ -0,0 +1,5 @@
+function openTicketing(event) {
+    var url = "https://oncogard30.sharepoint.com/:l:/s/ticketing/FKnIXnnjAb5Fnw5nUfWqks8BBlHciHQyNF1BkQzLkhXDPw?nav=MDUyZjg2NWQtNDE0Ni00MDcyLTg4ZTYtY2U4OGQ2YmM4ZThj";
+    Office.context.ui.openBrowserWindow(url);
+    event.completed();
+}
