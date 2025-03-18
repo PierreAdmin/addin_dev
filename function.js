725ce050ad1cfdc3ed440121587e73c1a079b15f
PierreAdmin <40363564+PierreAdmin@users.noreply.github.com>
2025-03-18 11:26:07 +0100

    Update function.js
    Update function.js

--- function.js
+++ function.js
@@ -1,7 +1,4 @@
-// Initialize the Office Add-in.
-Office.onReady(() => {
-  // If needed, Office.js is ready to be called
-});
+Office.initialize = function () {};
 
 function openTicketing(event) {
     var url = "https://oncogard30.sharepoint.com/:l:/s/ticketing/FKnIXnnjAb5Fnw5nUfWqks8BBlHciHQyNF1BkQzLkhXDPw?nav=MDUyZjg2NWQtNDE0Ni00MDcyLTg4ZTYtY2U4OGQ2YmM4ZThj";
