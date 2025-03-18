cbcefbdb25bf30cb4c427bf64a7e35dd31851bc4
PierreAdmin <40363564+PierreAdmin@users.noreply.github.com>
2025-03-18 11:41:27 +0100

    Update function.js

--- function.js
+++ function.js
@@ -1,4 +1,6 @@
-Office.initialize = function () {};
+Office.onReady(function() {
+    Office.actions.associate("openTicketingTool", openTicketingTool);
+});
 
 function openTicketing(event) {
     var url = "https://oncogard30.sharepoint.com/:l:/s/ticketing/FKnIXnnjAb5Fnw5nUfWqks8BBlHciHQyNF1BkQzLkhXDPw?nav=MDUyZjg2NWQtNDE0Ni00MDcyLTg4ZTYtY2U4OGQ2YmM4ZThj";
