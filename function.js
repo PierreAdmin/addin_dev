816f0c21ebf22d09911ddbf256135e13fc4581eb
PierreAdmin <40363564+PierreAdmin@users.noreply.github.com>
2025-03-19 14:16:48 +0100

    Update function.js

--- function.js
+++ function.js
@@ -3,7 +3,7 @@ Office.onReady(function() {
 });
 
 function openTicketingTool(event) {
-    var url = "https://oncogard30.sharepoint.com/sites/ticketing";
+    var url = "https://oncogard30.sharepoint.com/:u:/s/ticketing/EbRCh-ruOYtOoPh2rkqYTPMB3nHrH_7An8_YfxWeLGNubA?e=082PHs";
     Office.context.ui.openBrowserWindow(url);
     event.completed();
 }
