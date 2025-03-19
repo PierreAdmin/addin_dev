aa69c34a1b8688485953984d607fda967f4ebb4a
PierreAdmin <40363564+PierreAdmin@users.noreply.github.com>
2025-03-19 14:01:51 +0100

    Update function.js

--- function.js
+++ function.js
@@ -2,7 +2,7 @@ Office.onReady(function() {
     Office.actions.associate("openTicketingTool", openTicketingTool);
 });
 
-function openTicketing(event) {
+function openTicketingTool(event) {
     var url = "https://oncogard30.sharepoint.com/:l:/s/ticketing/FKnIXnnjAb5Fnw5nUfWqks8BBlHciHQyNF1BkQzLkhXDPw?nav=MDUyZjg2NWQtNDE0Ni00MDcyLTg4ZTYtY2U4OGQ2YmM4ZThj";
     Office.context.ui.openBrowserWindow(url);
     event.completed();
